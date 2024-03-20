527fb473d29d2a8f21b7890787807dbac048d5e9
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2024-03-20 03:51:43 -0700

    2. Console improvements

--- tests/test_models/test_console.py
+++ tests/test_models/test_console.py
@@ -37,7 +37,41 @@ class TestConsoleCreate(unittest.TestCase):
                             "name=\"Another test\" "
                             "description=\"Test description\"")
         self.assertTrue(mock_stdout.getValue().strip())
-        self.assertIn("BaseModel", mock_stdout.getvalue())
+        self.assertIn("BaseModel", mock_stdout.getvalue(i))
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_create_invalid_params(self, mock_stdout):
+        """Tests object creation with invalid parameters"""
+        self.console.oncmd("create BaseModel id=234")
+        self.assertFalse(mock_stdout.getvalue().strip())
+
+        self.console.onecmd("create BaseModel name\"Test\" invalid_param")
+        self.assertFalse(mock_stdout.getvalue().strip())
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_create_missing_class_name(self, mock_stdout):
+        """Tests object creation with missng class name"""
+        self.console.onecmd("create")
+        self.assertIn(
+                "** class name missing **", mock_stdout.getvalue().strip())
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_create_nonexistence_class(self, mock_stdout):
+        """Tests object creation with nonexistent class"""
+        self.console.onecmd("createInvalidClass name=\"Object\"")
+        self.assertIn(
+                "** class doesn't exist **", mock_stdout.getvalue().strip())
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_create_invalid_data_types(self, mock_stdout):
+        """Tests object creation with invalid data types"""
+        self.console.onecmd("create BaseModel name=\"Test\" id=\"invalid+is\"")
+        self.assertFalse(mock_stdout.getvalue().strip())
+
+        self.console.onecmd("create BaseModel name=\"Invalid Model\" "
+                            "number_rooms==\"Two\"")
+        self.assertFalse(mock_stdout.getvalue().strip())
+
 
 if __name__ == '__main__':
     unittest.main()
